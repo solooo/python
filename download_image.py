85655fa6b89285f97fae44f62269114713cfbad3
solooo <pjian108@gmail.com>
2016-06-23 14:45:07 +0800

    爬虫下载图片

--- download_image.py
+++ download_image.py
@@ -3,6 +3,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import os
 
 
 
@@ -12,15 +13,30 @@ def get_page_html(url):
     return response.text
 
 def get_imgurl(html):
-    pattern = re.compile(r'.+"middleURL":"(http://.+)".+')
-    return pattern.match(html)
+    pattern = re.compile(r'"objURL":"(http://.+)"')
+    return pattern.findall(html)
+
+def save_image(img_url_list):
+    img_dir = 'D:\\images'
+    os.mkdir(img_dir)
+    i = 1
+    for u in img_url_list:
+        print('正在下载第%s张' % i)
+        resp = requests.get(u)
+        file = os.path.join(img_dir, str(i) + '.jpg')
+        i = i + 1
+        with open(file, 'wb') as f:
+            f.write(resp.content)
+            f.close()
+        
 
 if __name__ == '__main__':
+    print('开始下载...')
     url = 'http://image.baidu.com/search/index?tn=baiduimage&ipn=r&ct=201326592&cl=2&lm=-1&st=-1&fr=&sf=1&fmq=1461833981476_R&pv=&ic=0&nc=1&z=&se=1&showtab=0&fb=0&width=&height=&face=0&istype=2&ie=utf-8&word=%E5%A3%81%E7%BA%B8'
     html_text = get_page_html(url)
     imgs = get_imgurl(html_text)
-    print(html_text)
-    print(imgs.goups())
+    save_image(imgs)
+    print('下载完成！')
 
 #soup = BeautifulSoup(response.text, 'html.parser')
 
--- re_test.py
+++ re_test.py
@@ -11,8 +11,35 @@ html = '''
                                 "pageNum":19,
                                 "objURL":"http://a.hiphotos.baidu.com/zhidao/pic/item/0ff41bd5ad6eddc4f995afa438dbb6fd5266337a.jpg",
                                 "fromURL":"http://zhidao.baidu.com/question/547157576.html",
+"os":"746439172,4060812556",
+                                "simid":"3438721281,300189250",
+                                "pi":"0",
+                                "adType":"0",
+                                "setDowloadURL":"",
+                                "setTittle":"",
+                                "DecorateCompanyName":"",
+                                "DecorateCompanyLocation":"",
+                                "DecorateWantuUrl":"",
+                                "personalized":"0"
+                }
+                ,                                {
+                                "thumbURL":"http://img5.imgtn.bdimg.com/it/u=99980900,631435245&fm=21&gp=0.jpg",
+                                "middleURL":"http://img5.imgtn.bdimg.com/it/u=99980900,631435245&fm=21&gp=0.jpg",
+                                "largeTnImageUrl":"",
+                                "hasLarge" : true,// 0,
+                                "hoverURL":"http://img5.imgtn.bdimg.com/it/u=99980900,631435245&fm=23&gp=0.jpg",
+                                "pageNum":27,
+                                "objURL":"http://www.deskcar.com/desktop/else/20101022103026/8.jpg",
+                                "fromURL":"http://www.deskcar.com/html/6346/8.shtml",
+                                "fromURLHost":"www.deskcar.com",
+                                "currentIndex":"",
+                                "width":1280,
+                                "height":1024,
+                                "type":"jpg",
+                                "filesize":"",
 '''
 
-pattern = re.compile('')
-s = pattern.match(html)
-type(s)
+pattern = re.compile(r'"middleURL":"(http://.+)"')
+s = pattern.findall(html)
+if s:
+    print(s)
